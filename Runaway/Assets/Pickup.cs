20bcf4d37ccbe507184c54f19b4be28466b4a756
DropBear25 <michelle25@live.co.uk>
2021-03-23 16:06:36 +0000

    Inventroy

---
+++ Runaway/Assets/Pickup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+
+
+    private Inventory inventory;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectsWithTag("Player").GetComponent<Inventory>();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
